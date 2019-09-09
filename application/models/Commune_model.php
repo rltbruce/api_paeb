22b5cbad28401b6fe66eba76409d0f4ff06189f0
rltbruce <brucetarafina@gmail.com>
2019-09-09 13:58:13 +0400

    first commit api

---
+++ application/models/Commune_model.php
@@ -0,0 +1,68 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Commune_model extends CI_Model {
+    protected $table = 'commune';
+
+    public function add($commune) {
+        $this->db->set($this->_set($commune))
+                            ->insert($this->table);
+        if($this->db->affected_rows() === 1)
+        {
+            return $this->db->insert_id();
+        }else{
+            return null;
+        }                    
+    }
+    public function update($id, $commune) {
+        $this->db->set($this->_set($commune))
+                            ->where('id', (int) $id)
+                            ->update($this->table);
+        if($this->db->affected_rows() === 1) {
+            return true;
+        }else{
+            return null;
+        }                      
+    }
+    public function _set($commune) {
+        return array(
+            'code'           =>      $commune['code'],
+            'nom'            =>      $commune['nom'],
+            'id_district'    =>      $commune['id_district']                       
+        );
+    }
+    public function delete($id) {
+        $this->db->where('id', (int) $id)->delete($this->table);
+        if($this->db->affected_rows() === 1) {
+            return true;
+        }else{
+            return null;
+        }  
+    }
+    public function findAll() {
+        $result =  $this->db->select('*')
+                        ->from($this->table)
+                        ->order_by('nom')
+                        ->get()
+                        ->result();
+        if($result)
+        {
+            return $result;
+        }else{
+            return null;
+        }                 
+    }
+    public function findById($id) {
+        $result =  $this->db->select('*')
+                        ->from($this->table)
+                        ->where("id", $id)
+                        ->order_by('id', 'asc')
+                        ->get()
+                        ->result();
+        if($result) {
+            return $result;
+        }else{
+            return null;
+        }                 
+    }	
+    
+}
---
+++ application/models/Region_model.php
@@ -0,0 +1,93 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Region_model extends CI_Model
+{
+    protected $table = 'region';
+
+
+    public function add($region)
+    {
+        $this->db->set($this->_set($region))
+                            ->insert($this->table);
+        if($this->db->affected_rows() === 1)
+        {
+            return $this->db->insert_id();
+        }else{
+            return null;
+        }                    
+    }
+
+
+    public function update($id, $region)
+    {
+        $this->db->set($this->_set($region))
+                            ->where('id', (int) $id)
+                            ->update($this->table);
+        if($this->db->affected_rows() === 1)
+        {
+            return true;
+        }else{
+            return null;
+        }                      
+    }
+
+    public function _set($region)
+    {
+        return array(
+            'code'       =>      $region['code'],
+            'nom'        =>      $region['nom'],                       
+        );
+    }
+
+
+    public function delete($id)
+    {
+        $this->db->where('id', (int) $id)->delete($this->table);
+        if($this->db->affected_rows() === 1)
+        {
+            return true;
+        }else{
+            return null;
+        }  
+    }
+
+    public function findAll()
+    {
+        $result =  $this->db->select('*')
+                        ->from($this->table)
+                        ->order_by('nom')
+                        ->get()
+                        ->result();
+        if($result)
+        {
+            return $result;
+        }else{
+            return null;
+        }                 
+    }
+
+    public function findById($id)
+    {
+        $this->db->where("id", $id);
+        $q = $this->db->get($this->table);
+        if ($q->num_rows() > 0) {
+            return $q->row();
+        }
+        return null;
+    }
+    public function findByIdtab($id)
+    {   $result =  $this->db->select('*')
+                        ->from($this->table)
+                        ->where("id", $id)
+                        ->get()
+                        ->result();
+        if($result)
+        {
+            return $result;
+        }
+        else
+        {
+            return null;
+        }                 
+    }    
+}
